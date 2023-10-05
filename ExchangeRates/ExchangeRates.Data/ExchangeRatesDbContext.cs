f5755297ecae5169c495d9184b3144a83ef91ffd
Grozev, Dimitar <dimitar.grozev@zuhlke.com>
2023-10-05 23:14:54 +0300

    Add data layer

---
+++ ExchangeRates/ExchangeRates.Data/ExchangeRatesDbContext.cs
@@ -0,0 +1,19 @@
+﻿using ExchangeRates.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeRates.Data
+{
+    public class ExchangeRatesDbContext: DbContext
+    {
+        public DbSet<ExchangeRate> ExchangeRates { get; set; }
+
+        public ExchangeRatesDbContext(DbContextOptions<ExchangeRatesDbContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ ExchangeRates/ExchangeRates.Data/Models/ExchangeRate.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExchangeRates.Data.Models
+{
+    public class ExchangeRate
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public int? Timestamp { get; set; }
+
+        public string? Base { get; set; }
+
+        public DateTime? Date { get; set; }
+
+        public string RatesJson { get; set; }
+    }
+}
